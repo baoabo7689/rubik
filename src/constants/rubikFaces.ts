15cbdd4f9a50f9afad3f760f581518b090d76d85
baoabo7689 <baoabo7689@gmail.com>
2026-02-22 06:05:54 +0000

    Deploying to gh-pages from @ baoabo7689/rubik@d38acfe10a765be3f7a2931b997e74b19ddfad26 🚀

--- src/constants/rubikFaces.ts
+++
@@ -1,3 +0,0 @@
-export const rubikFaceNames = ['U', 'D', 'R', 'L', 'F', 'B'] as const;
-
-export type RubikFaceName = (typeof rubikFaceNames)[number];
--- src/models/RubikCellModel.ts
+++
@@ -1,7 +0,0 @@
-export interface RubikCellModel {
-  color: string | null;
-}
-
-export function createDefault(color: string | null = null): RubikCellModel {
-  return { color };
-}
